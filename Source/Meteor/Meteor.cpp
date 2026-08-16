97b2db112733e9c64bdaa0a0a4f9ebb66027d51e
Chris Trombly <christrombly001@hotmail.com>
2026-08-16 14:11:23 -0400

    C++ UE Hello World message
    - Plugins added, failed, will be removed
    - Changed into C++ project
    - Simple hello world message

---
+++ Source/Meteor/Meteor.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Meteor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Meteor, "Meteor" );
